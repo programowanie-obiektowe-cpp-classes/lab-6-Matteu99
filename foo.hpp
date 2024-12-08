e71e24d5c19c82b35216e87b7482922f79fd46d9
Matteu99 <mateusz.wawrzyniak.1@gmail.com>
2024-12-08 20:02:11 +0100

    zej5

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,20 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+std::vector<char> foo(std::list<Human>& people)
 {
-    std::vector< char > retval(people.size());
+    std::vector<char> retval(people.size());
 
-    // Twoja implementacja tutaj
+    // Inkrementacja wieku przez wywołanie metody `birthday`.
+    std::for_each(people.begin(), people.end(), [](Human& person) {
+        person.birthday();
+    });
+
+    // Wypełnienie zwracanego wektora zgodnie z warunkiem i w odwrotnej kolejności.
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person) {
+        return person.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
+
